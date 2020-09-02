f5b289f92f29b352eb029f234ab8267ad4a8a53f
rahuljha661 <rahuljha12527@gmail.com>
2020-09-02 20:45:42 +0530

    adding postslist in other component

---
+++ src/components/PostsList.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+
+
+class PostsList extends Component {
+
+    render() {
+        const {posts}=this.props;
+        return (
+            <div className="posts-list">
+            {posts.map((post) => (
+              <div className="post-wrapper" key={post._id}>
+                <div className="post-header">
+                  <div className="post-avatar">
+                    <img
+                      src="https://image.flaticon.com/icons/svg/3011/3011270.svg"
+                      alt="user-pic"
+                    />
+                    <div>
+                      <span className="post-author">{post.user.name}</span>
+                      <span className="post-time">a min ago</span>
+                    </div>
+                  </div>
+                  <div className="post-content">{post.content}</div>
+  
+                  <div className="post-actions">
+                    <div className="post-like">
+                      <img
+                        src="https://image.flaticon.com/icons/svg/900/900397.svg"
+                        alt="likes-icon"
+                      />
+                      <span>{post.likes.length}</span>
+                      <div className="post-comments-icom">
+                        <img
+                          src="https://www.flaticon.com/premium-icon/icons/svg/2939/2939459.svg"
+                          alt="likes-icon"
+                        />
+                        <span>{post.comments.length}</span>
+                      </div>
+                    </div>
+  
+                   
+                  </div>
+                  <div className="post-comment-box">
+                         <input placeholder="Start typing a comment"/>
+                    </div>
+                
+                  <div className="post-comments-list">
+                      <div className="post-comments-item">
+                        <div className="post-comment-header">
+                              <span className="post-comment-author">Rahul</span>
+                              <span className="post-comment-time">a minute ago</span>
+                              <span className="post-comment-likes">22</span>
+                        </div>
+                         
+                         <div className="post-comment-content">
+                               Random comment
+                         </div>
+                      </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        );
+    }
+}
+
+export default PostsList;
