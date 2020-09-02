d60ee8d7eefdcb06c87f9db9e1d067d9675e6104
rahuljha661 <rahuljha12527@gmail.com>
2020-09-02 17:01:26 +0530

    create action store reducer and index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { configureStore } from './store';
 
 
+const store=configureStore();
+console.log("store",store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
